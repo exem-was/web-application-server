b0dd73162e7ee4deed370b0a02886e9af6dbbb26
cwparkexem <cwparkexem@gmail.com>
2022-03-18 17:01:50 +0900

    feat: 요구사항 2 - get 방식으로 회원가입

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Map;
+
 public class User {
     private String userId;
     private String password;
@@ -11,6 +13,15 @@ public class User {
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public static User from(Map<String, String> requestParameters) {
+        return new User(
+                requestParameters.get("userId"),
+                requestParameters.get("password"),
+                requestParameters.get("name"),
+                requestParameters.get("email")
+                );
     }
 
     public String getUserId() {
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,16 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Map;
 
+import com.google.common.base.Charsets;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,14 +27,44 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream();
              OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
+            String line = br.readLine();
+            String requestPath = getRequestPath(line);
+            log.debug("line : {}, request page : {}", line, requestPath);
+            String requestResource = getRequestResource(requestPath);
+            Map<String, String> requestParameters = getRequestParameters(requestPath);
+
+            while (!line.equals("")) {
+                line = br.readLine();
+                log.debug("line: {}", line);
+            }
+            if (requestResource.equals("/user/create")) {
+                User createdUser = User.from(requestParameters);
+                log.info("create user: {}", createdUser);
+            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private Map<String, String> getRequestParameters(String requestPath) {
+        if (requestPath.indexOf("?") == -1) {
+            return Collections.emptyMap();
+        }
+        String parameters = requestPath.split("\\?")[1];
+        return HttpRequestUtils.parseQueryString(parameters);
+    }
+
+    private String getRequestResource(String requestPath) {
+        return requestPath.split("\\?")[0];
+    }
+
+    private String getRequestPath(String line) {
+        return line.split("\\s")[1];
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
