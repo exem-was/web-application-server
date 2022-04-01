b7ec75e340f1b2117cfd99cb4cffc2157e0a8f3c
cwparkexem <cwparkexem@gmail.com>
2022-04-01 09:59:02 +0900

    feat: 요구사항 6 - 유저 목록 반환하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,7 @@ import util.IOUtils;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
@@ -38,10 +39,14 @@ public class RequestHandler extends Thread {
             String requestPath = getRequestPath(line);
             log.debug("line : {}, request page : {}", line, requestPath);
             String requestResource = getRequestResource(requestPath);
+            boolean logined = false;
 
             while (!line.equals("")) {
                 line = br.readLine();
                 parseContentLength(line);
+                if (line.contains("Cookie")) {
+                    logined = isLogin(line);
+                }
                 log.debug("line: {}", line);
             }
 
@@ -65,13 +70,61 @@ public class RequestHandler extends Thread {
                     response302WithLoginSuccess(dos, "/index.html");
                 }
             }
+            if (requestResource.equals("/user/list")) {
+                if (!logined) {
+                    requestPath = "/user/login.html";
+                }
+                if (logined) {
+                    responseUserListPage(dos);
+                }
+            }
 
             byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
-            response200Header(dos, body.length);
+            response200Header(dos, body.length, parseContentType(requestPath));
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String parseContentType(String requestPath) {
+        if (requestPath.endsWith(".html")) {
+            return "text/html";
+        }
+        if (requestPath.endsWith(".css")) {
+            return "text/css";
+        }
+        if (requestPath.endsWith(".js")) {
+            return "text/javascript";
+        }
+        return "text/plain";
+    }
+
+    private void responseUserListPage(DataOutputStream dos) {
+        Collection<User> users = DataBase.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table border='1'>");
+        for (User user : users) {
+            sb.append("<br>");
+            sb.append("<td>" + user.getUserId() + "</td>");
+            sb.append("<td>" + user.getName() + "</td>");
+            sb.append("<td>" + user.getEmail() + "</td>");
+            sb.append("</br>");
+        }
+        sb.append("</table>");
+        byte[] bytes = sb.toString().getBytes();
+        response200Header(dos, bytes.length, "text/html");
+        responseBody(dos, bytes);
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String logined = cookies.get("logined");
+        if (logined == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(logined);
     }
 
     private void response302WithLoginSuccess(DataOutputStream dos, String redirectPath) {
@@ -121,10 +174,10 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(format("Content-Type: %s;charset=utf-8\r\n", contentType));
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
